525137f61c9cc5e9d1e7aa74bda5a378536be1c3
Nguyen Tran Khanh Phung <phung.nguyentran05@gmail.com>
2026-06-09 13:12:21 +0700

    Implement loyalty customer discount feature

--- discount.py
+++ discount.py
@@ -1,4 +1,7 @@
-def calculate_discount(total):
+def calculate_discount(total,order_amount_new):
     if total >= 50000000:
+        return 0.1
+    return 0
+    if total + order_amount_new >= 50000000:
         return 0.1
     return 0
